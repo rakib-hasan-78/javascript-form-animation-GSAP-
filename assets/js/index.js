db92104bc06fb139b8efcab883cda2c667faf4d1
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-08-14 14:22:52 +0600

    line animation 90% done

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,26 @@ textAnimation('.title-word-one');
 gsap.to('.letter', {display:'inline-block'});
 gsap.fromTo('.letter', {y:'100%', opacity:0}, {y:0, opacity:1, delay:1, stagger:.05,ease:'back.out(3)', yoyo:true, repeat:-1, repeatDelay:.5});
 
+// *  form animation --->
+
+const containers = document.querySelectorAll('.input-container');
+const form = document.querySelector('form');
+const tl = gsap.timeline({defaults:{ duration:.75}});
+
+const start = "M0 0.5C0 0.5 13.5 0.5 149.5 0.5C285.5 0.5 300 0.5 300 0.5";
+const end = "M1 0.5C1 0.5 26.5 11 149 11C271.5 11 302.5 0.5 302.5 0.5" ;
+
+containers.forEach((container) => {
+    const input = container.querySelector('.input');
+    const line = container.querySelector('.elastic-line');
+    const placeholder = container.querySelector('.input-placeholder');
+    
+    input.addEventListener('click', () => {
+        if (!input.value) {
+            tl.fromTo(line, {attr: {d:start}}, {attr:{d:end}, ease: "power2.out"})
+        }
+    })
+})
+
+
+
